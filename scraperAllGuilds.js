1334697f40e2a621bd1c70c94e3a2a3ab3736459
Jorge Adriel <jorgeadriel03@gmail.com>
2025-06-15 23:52:10 -0400

    Configura Puppeteer para usar Chromium con args --no-sandbox y --disable-setuid-sandbox

--- scraperAllGuilds.js
+++ scraperAllGuilds.js
@@ -6,8 +6,10 @@ async function scrapeAllGuilds() {
 
         const browser = await puppeteer.launch({
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            executablePath: '/usr/bin/chromium-browser',
+          });
+          
 
         const page = await browser.newPage();
 
--- scraperGuild.js
+++ scraperGuild.js
@@ -4,8 +4,10 @@ async function scrapeGuildData() {
     try {
         const browser = await puppeteer.launch({
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            executablePath: '/usr/bin/chromium-browser',
+          });
+          
 
         const page = await browser.newPage();
 
